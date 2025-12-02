0f226bd95ec6e8f8967e9a178801ea0720e992a9
dodam24 <dodam0724@gmail.com>
2025-12-02 19:53:38 +0900

    Section 2: useMutation으로 포스팅 삭제하기

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -10,6 +10,12 @@ export function Posts() {
     const [selectedPost, setSelectedPost] = useState(null);
 
     const queryClient = useQueryClient();
+
+    const deleteMutation = useMutation({
+        mutationFn: (postId) => deletePost(postId),
+    });
+
+    // deleteMutation.mutate
 
     useEffect(() => {
         if (currentPage < maxPostPage) {
@@ -69,7 +75,7 @@ export function Posts() {
                 </button>
             </div>
             <hr />
-            {selectedPost && <PostDetail post={selectedPost} />}
+            {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} />}
         </>
     );
 }
