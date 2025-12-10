3defa136c955b124f5ce6a2a82e624362ce18a1e
dodam24 <dodam0724@gmail.com>
2025-12-10 21:46:42 +0900

    Section 4: Fallback 데이터

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,8 +12,10 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-    // TODO: get data from server via useQuery
-    const { data } = useQuery({
+    const fallback: Treatment[] = [];
+
+    // get data from server via useQuery
+    const { data = fallback } = useQuery({
         queryKey: [queryKeys.treatments],
         queryFn: getTreatments,
     });
