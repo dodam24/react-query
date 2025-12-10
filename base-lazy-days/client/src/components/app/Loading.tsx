8693f801e41eb6cdaf65444833ac6873901f533c
dodam24 <dodam0724@gmail.com>
2025-12-10 22:00:35 +0900

    Section 4: useIsFetching - Centralized Fetching Indicator

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,25 +1,26 @@
 import { Spinner, Text } from "@chakra-ui/react";
+import { useIsFetching } from "@tanstack/react-query";
 
 export function Loading() {
-  // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
-  const display = isFetching ? "inherit" : "none";
+    // use React Query `useIsFetching` to determine whether or not to display
+    const isFetching = useIsFetching(); // 현재 진행 중인 쿼리 fetch 요청의 총 개수를 정수로 반환
+    const display = isFetching ? "inherit" : "none";
 
-  return (
-    <Spinner
-      thickness="4px"
-      speed="0.65s"
-      emptyColor="olive.200"
-      color="olive.800"
-      role="status"
-      position="fixed"
-      zIndex="9999"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      display={display}
-    >
-      <Text display="none">Loading...</Text>
-    </Spinner>
-  );
+    return (
+        <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="olive.200"
+            color="olive.800"
+            role="status"
+            position="fixed"
+            zIndex="9999"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            display={display}
+        >
+            <Text display="none">Loading...</Text>
+        </Spinner>
+    );
 }
