83c0c424a4e229d3973b50184cdd08ede11bd58a
dodam24 <dodam0724@gmail.com>
2025-12-20 14:50:17 +0900

    Feat: ESLint sort-imports 설치 및 적용

--- base-lazy-days/client/vite.config.js
+++ base-lazy-days/client/vite.config.js
@@ -2,26 +2,31 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import simpleImportSort from "vite-plugin-simple-import-sort";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    // this points to the setup file
-    setupFiles: "./src/setupTests.js",
-  },
-  server: {
-    // to match server expectation
-    port: 3000,
-    // exit if port 3000 is in use (to avoid CORS errors)
-    strict: true,
-  },
-  resolve: {
-    alias: {
-      "@shared": path.join(__dirname, "../shared/"),
-      "@": path.join(__dirname, "src/"),
+    plugins: [react(), simpleImportSort()],
+    rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
     },
-  },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        // this points to the setup file
+        setupFiles: "./src/setupTests.js",
+    },
+    server: {
+        // to match server expectation
+        port: 3000,
+        // exit if port 3000 is in use (to avoid CORS errors)
+        strict: true,
+    },
+    resolve: {
+        alias: {
+            "@shared": path.join(__dirname, "../shared/"),
+            "@": path.join(__dirname, "src/"),
+        },
+    },
 });
